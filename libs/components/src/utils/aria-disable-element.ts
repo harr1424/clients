3419b08a1437cd07fb78b0144520b2afae0b0868
Bryan Cunningham <bryan.cunningham@me.com>
2025-06-11 09:34:16 -0400

    add JSDoc comment for util function

--- libs/components/src/utils/aria-disable-element.ts
+++ libs/components/src/utils/aria-disable-element.ts
@@ -2,6 +2,11 @@ import { Signal, effect } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { fromEvent } from "rxjs";
 
+/**
+ * a11y helper util used to `aria-disable` elements as opposed to using the HTML `disabled` attr.
+ * - Removes HTML `disabled` attr and replaces it with `aria-disabled="true"`
+ * - Captures click events and prevents them from propagating
+ */
 export function ariaDisableElement(element: HTMLElement, isDisabled: Signal<boolean | undefined>) {
   effect(() => {
     if (element.hasAttribute("disabled") || isDisabled()) {
