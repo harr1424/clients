af08c0aa0a4c482ed8cd3319a32eba16c71b6c46
Thomas Avery <tavery@bitwarden.com>
2025-07-31 13:08:56 -0500

    Add unit test coverage for organization-billing service

--- libs/common/src/billing/services/organization-billing.service.spec.ts
+++ libs/common/src/billing/services/organization-billing.service.spec.ts
@@ -4,8 +4,12 @@ import { firstValueFrom, of } from "rxjs";
 import { ApiService } from "@bitwarden/common/abstractions/api.service";
 import { OrganizationApiServiceAbstraction as OrganizationApiService } from "@bitwarden/common/admin-console/abstractions/organization/organization-api.service.abstraction";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
-import { BillingApiServiceAbstraction } from "@bitwarden/common/billing/abstractions";
-import { ProductTierType } from "@bitwarden/common/billing/enums";
+import {
+  BillingApiServiceAbstraction,
+  PaymentInformation,
+  SubscriptionInformation,
+} from "@bitwarden/common/billing/abstractions";
+import { PaymentMethodType, ProductTierType } from "@bitwarden/common/billing/enums";
 import { OrganizationBillingService } from "@bitwarden/common/billing/services/organization-billing.service";
 import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
 import { EncryptService } from "@bitwarden/common/key-management/crypto/abstractions/encrypt.service";
@@ -15,6 +19,14 @@ import { SyncService } from "@bitwarden/common/platform/sync";
 // This import has been flagged as unallowed for this class. It may be involved in a circular dependency loop.
 // eslint-disable-next-line no-restricted-imports
 import { KeyService } from "@bitwarden/key-management";
+import { UserId } from "@bitwarden/user-core";
+
+import { OrganizationKeysRequest } from "../../admin-console/models/request/organization-keys.request";
+import { OrganizationResponse } from "../../admin-console/models/response/organization.response";
+import { EncString } from "../../key-management/crypto/models/enc-string";
+import { Utils } from "../../platform/misc/utils";
+import { SymmetricCryptoKey } from "../../platform/models/domain/symmetric-crypto-key";
+import { OrgKey } from "../../types/key";
 
 describe("BillingAccountProfileStateService", () => {
   let apiService: jest.Mocked<ApiService>;
@@ -146,6 +158,192 @@ describe("BillingAccountProfileStateService", () => {
 
       await firstValueFrom(sut.isBreadcrumbingPoliciesEnabled$(org));
       expect(configService.getFeatureFlag$).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("organization key creation methods", () => {
+    const mockUserId = Utils.newGuid() as UserId;
+    const organizationKeys = {
+      orgKey: new SymmetricCryptoKey(new Uint8Array(64)) as OrgKey,
+      publicKeyEncapsulatedOrgKey: new EncString("encryptedOrgKey"),
+      publicKey: "public-key",
+      encryptedPrivateKey: new EncString("encryptedPrivateKey"),
+    };
+    const encryptedCollectionName = new EncString("encryptedCollectionName");
+    const mockSubscription = {
+      organization: {
+        name: "Test Org",
+        businessName: "Test Business",
+        billingEmail: "test@example.com",
+        initiationPath: "Registration form",
+      },
+      plan: {
+        type: 0, // Free plan
+        passwordManagerSeats: 0,
+        subscribeToSecretsManager: false,
+        isFromSecretsManagerTrial: false,
+      },
+    } as SubscriptionInformation;
+    const mockResponse = { id: "org-id" } as OrganizationResponse;
+
+    beforeEach(() => {
+      keyService.makeOrgKey.mockResolvedValue([
+        organizationKeys.publicKeyEncapsulatedOrgKey,
+        organizationKeys.orgKey,
+      ]);
+      keyService.makeKeyPair.mockResolvedValue([
+        organizationKeys.publicKey,
+        organizationKeys.encryptedPrivateKey,
+      ]);
+      encryptService.encryptString.mockResolvedValueOnce(encryptedCollectionName);
+      i18nService.t.mockReturnValue("Default Collection");
+
+      organizationApiService.create.mockResolvedValue(mockResponse);
+    });
+
+    describe("purchaseSubscription", () => {
+      it("sets the correct organization keys on the organization creation request", async () => {
+        const subscriptionWithPayment = {
+          ...mockSubscription,
+          payment: {
+            paymentMethod: ["test-token", PaymentMethodType.Card],
+            billing: {
+              postalCode: "12345",
+              country: "US",
+            },
+          } as PaymentInformation,
+        } as SubscriptionInformation;
+        const result = await sut.purchaseSubscription(subscriptionWithPayment, mockUserId);
+
+        expect(keyService.makeOrgKey).toHaveBeenCalledWith(mockUserId);
+        expect(keyService.makeKeyPair).toHaveBeenCalledWith(organizationKeys.orgKey);
+        expect(encryptService.encryptString).toHaveBeenCalledWith(
+          "Default Collection",
+          organizationKeys.orgKey,
+        );
+        expect(organizationApiService.create).toHaveBeenCalledWith(
+          expect.objectContaining({
+            name: "Test Org",
+            businessName: "Test Business",
+            billingEmail: "test@example.com",
+            initiationPath: "Registration form",
+            planType: 0,
+            key: organizationKeys.publicKeyEncapsulatedOrgKey.encryptedString,
+            keys: new OrganizationKeysRequest(
+              organizationKeys.publicKey,
+              organizationKeys.encryptedPrivateKey.encryptedString!,
+            ),
+            collectionName: encryptedCollectionName.encryptedString,
+          }),
+        );
+        expect(apiService.refreshIdentityToken).toHaveBeenCalled();
+        expect(syncService.fullSync).toHaveBeenCalledWith(true);
+        expect(result).toBe(mockResponse);
+      });
+    });
+
+    describe("purchaseSubscriptionNoPaymentMethod", () => {
+      it("sets the correct organization keys on the organization creation request", async () => {
+        organizationApiService.createWithoutPayment.mockResolvedValue(mockResponse);
+
+        const result = await sut.purchaseSubscriptionNoPaymentMethod(mockSubscription, mockUserId);
+
+        expect(keyService.makeOrgKey).toHaveBeenCalledWith(mockUserId);
+        expect(keyService.makeKeyPair).toHaveBeenCalledWith(organizationKeys.orgKey);
+        expect(encryptService.encryptString).toHaveBeenCalledWith(
+          "Default Collection",
+          organizationKeys.orgKey,
+        );
+        expect(organizationApiService.createWithoutPayment).toHaveBeenCalledWith(
+          expect.objectContaining({
+            name: "Test Org",
+            businessName: "Test Business",
+            billingEmail: "test@example.com",
+            initiationPath: "Registration form",
+            planType: 0,
+            key: organizationKeys.publicKeyEncapsulatedOrgKey.encryptedString,
+            keys: new OrganizationKeysRequest(
+              organizationKeys.publicKey,
+              organizationKeys.encryptedPrivateKey.encryptedString!,
+            ),
+            collectionName: encryptedCollectionName.encryptedString,
+          }),
+        );
+        expect(apiService.refreshIdentityToken).toHaveBeenCalled();
+        expect(syncService.fullSync).toHaveBeenCalledWith(true);
+        expect(result).toBe(mockResponse);
+      });
+    });
+
+    describe("startFree", () => {
+      it("sets the correct organization keys on the organization creation request", async () => {
+        const result = await sut.startFree(mockSubscription, mockUserId);
+
+        expect(keyService.makeOrgKey).toHaveBeenCalledWith(mockUserId);
+        expect(keyService.makeKeyPair).toHaveBeenCalledWith(organizationKeys.orgKey);
+        expect(encryptService.encryptString).toHaveBeenCalledWith(
+          "Default Collection",
+          organizationKeys.orgKey,
+        );
+        expect(organizationApiService.create).toHaveBeenCalledWith(
+          expect.objectContaining({
+            name: "Test Org",
+            businessName: "Test Business",
+            billingEmail: "test@example.com",
+            initiationPath: "Registration form",
+            planType: 0,
+            key: organizationKeys.publicKeyEncapsulatedOrgKey.encryptedString,
+            keys: new OrganizationKeysRequest(
+              organizationKeys.publicKey,
+              organizationKeys.encryptedPrivateKey.encryptedString!,
+            ),
+            collectionName: encryptedCollectionName.encryptedString,
+          }),
+        );
+        expect(apiService.refreshIdentityToken).toHaveBeenCalled();
+        expect(syncService.fullSync).toHaveBeenCalledWith(true);
+        expect(result).toBe(mockResponse);
+      });
+    });
+
+    describe("restartSubscription", () => {
+      it("sets the correct organization keys on the organization creation request", async () => {
+        const subscriptionWithPayment = {
+          ...mockSubscription,
+          payment: {
+            paymentMethod: ["test-token", PaymentMethodType.Card],
+            billing: {
+              postalCode: "12345",
+              country: "US",
+            },
+          } as PaymentInformation,
+        } as SubscriptionInformation;
+
+        await sut.restartSubscription("org-id", subscriptionWithPayment, mockUserId);
+
+        expect(keyService.makeOrgKey).toHaveBeenCalledWith(mockUserId);
+        expect(keyService.makeKeyPair).toHaveBeenCalledWith(organizationKeys.orgKey);
+        expect(encryptService.encryptString).toHaveBeenCalledWith(
+          "Default Collection",
+          organizationKeys.orgKey,
+        );
+        expect(billingApiService.restartSubscription).toHaveBeenCalledWith(
+          "org-id",
+          expect.objectContaining({
+            name: "Test Org",
+            businessName: "Test Business",
+            billingEmail: "test@example.com",
+            initiationPath: "Registration form",
+            planType: 0,
+            key: organizationKeys.publicKeyEncapsulatedOrgKey.encryptedString,
+            keys: new OrganizationKeysRequest(
+              organizationKeys.publicKey,
+              organizationKeys.encryptedPrivateKey.encryptedString!,
+            ),
+            collectionName: encryptedCollectionName.encryptedString,
+          }),
+        );
+      });
     });
   });
 });
